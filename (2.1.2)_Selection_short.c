d7d8aa7697f2fb680e83b0a68d2dae9742b07e15
pranjalchoudhary30 <pranjalchoudhary30@gmail.com>
2026-02-10 22:05:57 +0530

    Create (2.1.2)_Selection_short.c

---
+++ (2.1.2)_Selection_short.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void selectionSort(int arr[], int n) {
+    int i, j, max_idx, temp;
+    for (i = 0; i < n - 1; i++) {
+        max_idx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] > arr[max_idx]) {
+                max_idx = j;
+            }
+        }
+        temp = arr[max_idx];
+        arr[max_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+int main() {
+    int n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    selectionSort(arr, n);
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
