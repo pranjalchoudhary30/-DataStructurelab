18060f788ba019ee3c32f5266afa463db3d12eaf
pranjalchoudhary30 <pranjalchoudhary30@gmail.com>
2026-04-08 10:25:34 +0530

    Create (7.1.1.)Binary_Search_Tree_Operations.c

---
+++ (7.1.1.)Binary_Search_Tree_Operations.c
@@ -0,0 +1,37 @@
+/*#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+};
+struct TreeNode* insertNode(   ) {
+}
+
+
+void inorderTraversal(  ) {
+
+}
+
+void preorderTraversal(  ) {
+
+}
+
+void postorderTraversal(  ) {
+
+}
+
+struct TreeNode* deleteNode(  ) {
+}
+
+int main() {
+    struct TreeNode* root = NULL;
+    int choice, data;
+
+    while (1) {
+        printf("1. Insert Node\n");
+        printf("2. In-Order Traversal\n");
+        printf("3. Pre-Order Traversal\n");
+        printf("4. Post-Order Traversal\n");
+}
